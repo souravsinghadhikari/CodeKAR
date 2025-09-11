7f4dae7863a9d92c794a6b64893956e2c6856042
Sourav Singh Adhikari <souravadhikari668@gmail.com>
2025-09-12 01:16:47 +0530

    database finalized and doxker is good to go

--- backend/master/server.js
+++ backend/master/server.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 5000;
 
 app.get('/health', (req, res) => {
-    res.json({ success: true, message: "Master service is running 🚀" });
+  res.status(200).json({ status: 'ok' });
+});
+
+app.get('/', (req, res) => {
+  res.send("changed  detected ?")
 });
 
 app.listen(PORT, () => {
